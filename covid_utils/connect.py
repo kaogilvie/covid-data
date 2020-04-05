81ba4f185b97aebabed44f9019be55afeef67f8c
Kyle Ogilvie <kogilvie@2u.com>
2020-04-05 15:58:43 -0400

    adding requirements file

--- covid_utils/connect.py
+++ covid_utils/connect.py
@@ -1,8 +1,11 @@
+import sqlalchemy
+import psycopg2 as pg
+
+
 def dbconn(creds):
     """
     Pass in raw SQL to this connection.
     """
-    import psycopg2 as pg
     conn = pg.connect(**creds)
     return conn
 
@@ -11,7 +14,6 @@ def pandas_dbconn(creds, ssl=False, redshift=False):
     DB is the name of the database as referred to in the creds dictionary.
     Defaults to postgres protocol; can force it to use the redshift-sqlalchemy shim library.
     """
-    import sqlalchemy
     if redshift is True:
         protocol = 'redshift+psycopg2://'
     else:
