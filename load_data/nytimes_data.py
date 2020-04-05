18ed4a6567954cb54681f6f361be918c17b317d4
Kyle Ogilvie <kogilvie@2u.com>
2020-04-05 15:46:31 -0400

    transfer to class structure to encompass all tables

--- load_data/nytimes_data.py
+++ load_data/nytimes_data.py
@@ -1,61 +1,76 @@
+import csv
+import os
+import logging
+
+import psycopg2
+from psycopg2.extras import DictCursor
+import pandas as pd
+
+from covid_utils import logs
+from covid_utils import connect
+from covid_utils import credentials
+
+class NTYDataLoader(object):
+    def __init__(self):
+        logs.configure_logging('NTYDataLoader')
+        self.logger = logging.getLogger()
+
+        self.schema = 'nytimes'
+        self.file_root = os.path.expanduser('~/Documents/github/nytimes-covid-data')
+
+        self.connect_to_postgres()
+
+    def connect_to_postgres(self):
+        self.logger.info("Connecting to postgres..")
+        self.pg_creds = credentials.get_postgres_creds()
+        self.cxn = connect.dbconn(self.pg_creds)
+        self.cursor = self.cxn.cursor(cursor_factory=DictCursor)
+        self.logger.info("Connected to postgres.")
+
+    def get_most_recent_date(self, table, date_column='date'):
+        self.logger.info(f"Appending new data to {table}. First getting most recent data...")
+        self.cursor.execute(f"SELECT max({date_column}) FROM {self.schema}.{table};")
+        self.recent_date = self.cursor.fetchall()[0][0]
+
+    def load_data(self, table, data_filename, date_column='date'):
+        if self.recent_date is None:
+            self.logger.info("Accessing full reload data..")
+            data_to_load = open(f'{self.file_root}/{data_filename}', 'r')
+            data_header = next(data_to_load).strip().split(',')
+
+            self.logger.info(f"Initializing full load of {table}...")
+            self.cursor.copy_from(data_to_load, f'{self.schema}.{table}', sep=',', null="", columns=data_header)
+            self.cxn.commit()
+
+            self.logger.info("Loaded table fully...")
+            self.cursor.execute(f"SELECT count(*) FROM {self.schema}.{table};")
+            cnt = self.cursor.fetchall()
+            self.logger.info(f'...meaning {cnt[0][0]} rows.')
+        else:
+            self.logger.info(f"Initializing incremental load of {table} new rows beginning with the day after {self.recent_date}.")
+
+            self.logger.info("Slicing data...")
+            full_data_df = pd.read_csv(f'{self.file_root}/{data_filename}')
+            insert_df = full_data_df[full_data_df[f'{date_column}'] > str(self.recent_date)]
+            self.logger.info(f"Going to append a df of {len(insert_df)} rows using pandas.")
+
+            self.logger.info("Connecting to Postgres via SQLAlchemy for pandas.")
+            self.pd_cxn = connect.pandas_dbconn(self.pg_creds)
+
+            self.logger.info("Appending rows...")
+            insert_df.to_sql(f'{table}', self.pd_cxn, schema=f'{self.schema}', if_exists='append', index=False, method='multi')
+
+            self.logger.info("Done appending.")
+
+
 if __name__ == "__main__":
-    import csv
-    import os
-    import logging
 
-    import psycopg2
-    from psycopg2.extras import DictCursor
-    import pandas as pd
+    tables_to_load = {
+        'cases_by_county': 'us-counties.csv',
+        'cases_by_state': 'us-states.csv'
+    }
 
-    from covid_utils import logs
-    from covid_utils import connect
-    from covid_utils import credentials
-
-    ## set up logging
-    logs.configure_logging('covid')
-    logger = logging.getLogger()
-
-    ## connect to postgres
-    logger.info("Connecting to postgres...")
-    pg_creds = credentials.get_postgres_creds()
-    cxn = connect.dbconn(pg_creds)
-    cursor = cxn.cursor(cursor_factory=DictCursor)
-    logger.info("Connected to postgres.")
-
-    ## data setup
-    nytimes_data_root = '~/Documents/github/nytimes-covid-data'
-    county_path = f'{nytimes_data_root}/us-counties.csv'
-    state_path = f'{nytimes_data_root}/us-states.csv'
-
-    logger.info("Appending new county data. First getting most recent data...")
-    cursor.execute("SELECT max(date) FROM nytimes.cases_by_county;")
-    fetched_data = cursor.fetchall()
-
-    if fetched_data[0][0] is None:
-        logger.info("Accessing full reload data..")
-        county_data = open(os.path.expanduser(county_path), 'r')
-        county_header = next(county_data).strip().split(',')
-
-        logger.info("Initializing full load of table...")
-        cursor.copy_from(county_data, 'nytimes.cases_by_county', sep=',', null="", columns=county_header)
-        cxn.commit()
-
-        logger.info("Loaded table fully...")
-        cursor.execute("SELECT count(*) FROM nytimes.cases_by_county;")
-        cnt = cursor.fetchall()
-        logger.info(f'...meaning {cnt[0][0]} rows.')
-    else:
-        logger.info(f"Initializing incremental load of new rows beginning with the day after {fetched_data[0][0]}.")
-
-        logger.info("Slicing data...")
-        county_df = pd.read_csv(county_path)
-        insert_df = county_df[county_df['date'] > str(fetched_data[0][0])]
-        logger.info(f"Going to append a df of {len(insert_df)} rows using pandas.")
-
-        logger.info("Connecting to Postgres via SQLAlchemy for pandas.")
-        pd_cxn = connect.pandas_dbconn(pg_creds)
-
-        logger.info("Appending rows...")
-        insert_df.to_sql('cases_by_county', pd_cxn, schema='nytimes', if_exists='append', index=False, method='multi')
-
-        logger.info("Done appending.")
+    nyt = NTYDataLoader()
+    for table, filename in tables_to_load.items():
+        nyt.get_most_recent_date(table)
+        nyt.load_data(table, filename)
