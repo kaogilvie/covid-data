14c4a3adeb676de285e7f916cce7f05887bcc8b2
Kyle Ogilvie <kogilvie@2u.com>
2020-05-24 12:55:57 -0400

    connect to remote db; create table if doesnt exist

--- covid_utils/credentials.py
+++ covid_utils/credentials.py
@@ -1,10 +1,21 @@
 import os
 
-def get_postgres_creds():
-    return {
-        'user': os.getenv('COVID_DB_USER'),
-        'password': os.getenv('COVID_DB_PASSWORD'),
-        'host': 'localhost',
-        'port': '5432',
-        'database': 'covid'
-    }
+def get_postgres_creds(local=True):
+    if local is False:
+        creds = {
+                'user': os.getenv('AWS_COVID_USER'),
+                'password': os.getenv('AWS_COVID_DB_PW'),
+                'host': os.getenv('AWS_COVID_HOST'),
+                'port': os.getenv('AWS_COVID_PORT'),
+                'database': os.getenv('AWS_COVID_DB')
+            }
+    else:
+        creds = {
+                'user': os.getenv('COVID_DB_USER'),
+                'password': os.getenv('COVID_DB_PASSWORD'),
+                'host': 'localhost',
+                'port': '5432',
+                'database': 'covid'
+        }
+
+    return creds
